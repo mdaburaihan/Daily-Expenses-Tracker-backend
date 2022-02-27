ee2cf99018a23828921edaa692cdd6d8b651c7e3
Md Abu Raihan <raihanabu003@gmail.com>
2022-02-27 23:43:14 +0530

    latest code

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-//const config = require('config');
 const dotenv = require('dotenv')
 dotenv.config()
 const session = require('express-session');
 const mongoose = require('mongoose');
-//const users = require('./routes/users');
 const expense = require('./routes/expense');
 const auth = require('./routes/auth');
 const autoIncrement = require('mongoose-auto-increment');
 const express = require('express');
-//const passport = require('passport');
-//const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const app = express();
 const cors = require('cors')
 const bodyParser = require('body-parser')
@@ -34,8 +30,6 @@ passport.serializeUser(function (user, cb) {
 passport.deserializeUser(function (obj, cb) {
     cb(null, obj);
 });
-// app.use(passport.initialize());
-// app.use(passport.session());
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -66,44 +60,8 @@ app.use((req, res, next) => {
     next();
 })
 
-
-// passport.serializeUser(function (user, cb) {
-//     cb(null, user);
-// });
-
-// passport.deserializeUser(function (obj, cb) {
-//     cb(null, obj);
-// });
-
-// passport.use(new GoogleStrategy({
-//     clientID: process.env.GOOGLE_CLIENT_ID,
-//     clientSecret: process.env.GOOGLE_CLIENT_SECRET_KEY,
-//     callbackURL: `${process.env.LOCAL_HOST_URL}auth/google/callback`
-// },
-// function (req, accessToken, refreshToken, profile, done) {
-//     // if user already exist in your dataabse login otherwise
-//     console.log("===profile===", profile)
-//     // save data and signup
-//     return done(null, profile);
-// }
-// ));
-
-//app.get('/auth/google', passport.authenticate('google', {scope: ['openid','profile', 'email']}));
-
-// app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/auth/fail'}),
-//     (req, res, next) => {
-//         console.log(req.user, req.isAuthenticated());
-//         res.send('user is logged in');
-//     })
-
-// app.get('/auth/fail', (req, res, next) => {
-//     res.send('user logged in failed');
-// });
-
 app.use('/auth', auth);
 app.use('/expense', expense);
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port,  () => console.log(`Listening to port ${port}...`));
